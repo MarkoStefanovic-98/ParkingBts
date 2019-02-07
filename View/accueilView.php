4a6114ec2b22ffe506e06f701c8a8aff1690fcb1
MarkoStefanovic-98 <markostef@hotmail.fr>
2019-02-07 17:14:14 +0100

    Ajout du squelette avec accueil et login

---
+++ View/accueilView.php
@@ -0,0 +1,5 @@
+<center><h1>
+
+Bienvenue sur le site de réservation parking pour l'épreuve E4 !
+
+</h1></center>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+  require "modele/connexion.php";  
+  
+  define('WEBROOT', dirname(__FILE__));
+  define('BASE_URL', dirname($_SERVER['SCRIPT_NAME']));
+  define('ROOT', dirname(WEBROOT));
+  define('DS', DIRECTORY_SEPARATOR);
+  define('CORE',ROOT.DS.'core');
+
+if(!isset($_GET['p']) || $_GET['p'] == "")
+{
+        $page = "accueil";
+}
+else
+{
+    if(!file_exists("controller/".$_GET['p'].".php"))
+        {
+            $page = 404;
+        }
+        else
+            $page = $_GET['p'];
+    }
+    ob_start();//suspend l'affichage
+    require "controller/".$page.".php";
+    $content = ob_get_contents();//recuperer ce qui n'a pas ete affiché
+    ob_end_clean();//reprend l'affichage
+
+require "layout.php";
+?>
